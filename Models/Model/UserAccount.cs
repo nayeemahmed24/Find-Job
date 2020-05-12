5c754cad6b1acac01327c51e2b8b94c7e326e2bc
Nayeem Ahmed <nayeemahmed24@gmail.com>
2020-05-13 01:11:57 +0600

    Refactoring from Scratch

---
+++ Models/Model/UserAccount.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Models
+{
+    public class UserAccount : IdentityUser
+    {
+        [Required] public string Name { get; set; }
+    }
+}
